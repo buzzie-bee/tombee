ba3fbe175de2ee83c2064b57750497fecd60d32a
buzzie-bee <39500959+buzzie-bee@users.noreply.github.com>
2021-02-27 15:32:42 +0100

    Tweaked button to change text color too.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,17 @@ export const Button = ({
   onClick,
   children,
   color = 'blue-600',
+  hoverTextColor = 'white',
 }: {
   onClick: () => void;
   children: JSX.Element;
   color?: string;
+  hoverTextColor?: string;
 }) => {
   return (
     <button
       onClick={onClick}
-      className={`mx-auto my-2 mr-6 w-full sm:w-28 sm:my-4 border-2 border-${color} rounded-lg font-bold text-${color} px-4 py-2 transition duration-300 ease-in-out hover:bg-${color} hover:text-white`}
+      className={`mx-auto my-2 mr-6 w-full sm:w-28 sm:my-4 border-2 border-${color} rounded-lg font-bold text-${color} px-4 py-2 transition duration-300 ease-in-out hover:bg-${color} hover:text-${hoverTextColor}`}
     >
       {children}
     </button>
